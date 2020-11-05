87f7e15043f47d42ddf26209b5e6ca420f484012
Stas Davydov <davidovsv@yandex.ru>
2020-11-06 00:59:59 +0300

    setup.py is added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ca-taxes",
+    version="1.0",
+    description="Django App for handling California Taxes",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stasdavydov/ca-taxes",
+    author="Stas Davydov",
+    author_email="davidovsv@yandex.ru",
+    license="Unlicense License",
+    classifiers=[
+        "License :: OSI Approved :: Unlicense License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["ca_taxes"],
+    include_package_data=True,
+    install_requires=["requests", "Django"],
+)
